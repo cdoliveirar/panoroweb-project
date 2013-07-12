0ea054149e4e141a6c88c374bededce95ce218be
Carlos Olvieira <cdoliveirar@gmail.com>
2013-07-12 12:26:10 -0300

    settings configurations

---
+++ settingsproduction/wsgi.py
@@ -0,0 +1,32 @@
+"""
+WSGI config for PanoroWeb project.
+
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
+
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
+"""
+import os, sys
+
+#where is the project
+sys.path.append('C:/Program Files (x86)/Apache Software Foundation/Apache2.2/htdocs/PanoroWeb')
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PanoroWeb.settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
